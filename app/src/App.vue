75f14890e9c37c5d69f1a617160ac0bacd238fb5
Ian Ndegwa <ndegwaian241@gmail.com>
2024-11-07 19:07:07 +0300

    message

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,17 +1,9 @@
 <template>
-  <div class="min-h-screen bg-white">
-    <Header />
-    <div class="mt-4">
-      <RouterView />
-    </div>
-    <Footer />
-  </div>
+    <RouterView />
 </template>
 
 <script setup>
 import { RouterView } from "vue-router";
-import Header from "./components/Header.vue";
-import Footer from "./components/Footer.vue";
 </script>
 
 <style scoped></style>
--- app/src/components/Footer.vue
+++ app/src/components/Footer.vue
@@ -1,7 +1,10 @@
 <template>
-  <div>Footer</div>
+  <footer
+    class="mt-8 border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white"
+  >
+    <div class="container p-12 flex justify-between">
+      <div class="text-white text-3xl font-bold">LOGO</div>
+      <p class="text-slate-600">All rights reserved.</p>
+    </div>
+  </footer>
 </template>
-
-<script setup></script>
-
-<style lang="scss" scoped></style>
